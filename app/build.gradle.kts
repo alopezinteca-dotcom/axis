4594f766aab1daf2d7710fd480dfcfcf6c6380d6
alopezinteca-dotcom <alopezinteca@gmail.com>
2026-04-09 19:49:12 +0000

    Add Android Compose template for AXIS

---
+++ app/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+}
+
+android {
+    namespace = "vtsen.hashnode.dev.newemptycomposeapp"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "vtsen.hashnode.dev.newemptycomposeapp"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    packaging {
+        resources {
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+
+    implementation(libs.bundles.androidx.lifeycle)
+
+    implementation(libs.androidx.activity.compose)
+
+    implementation(libs.bundles.compose.ui)
+
+    implementation(libs.accompanist.systemuicontroller)
+
+    implementation(libs.androidx.splash.screen)
+
+    implementation(libs.vinchamp77.buildutils)
+}
