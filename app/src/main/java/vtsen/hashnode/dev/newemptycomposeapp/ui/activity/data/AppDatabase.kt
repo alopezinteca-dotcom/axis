d8e88febd998f5e5777bc36e832d1d915c4e5320
alopezinteca-dotcom <alopezinteca@gmail.com>
2026-04-11 13:11:40 +0200

    Update AppDatabase.kt

--- app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/activity/data/AppDatabase.kt
+++ app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/activity/data/AppDatabase.kt
@@ -6,35 +6,26 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-/* =========================================================
-   ROOM DATABASE · AXIS
-   ========================================================= */
-
 @Database(
     entities = [TravelEntity::class],
-    version = 1,
-    exportSchema = false
+    version = 1
 )
-@TypeConverters(Converters::class) // <-- ENCHUFAMOS EL TRADUCTOR AQUÍ
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun travelDao(): TravelDao
 
     companion object {
-
         @Volatile
-        private var INSTANCE: AppDatabase? = null
+        private var instance: AppDatabase? = null
 
         fun getInstance(context: Context): AppDatabase {
-            return INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
+            return instance ?: synchronized(this) {
+                instance ?: Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "axis.db"
-                ).build()
-
-                INSTANCE = instance
-                instance
+                ).build().also { instance = it }
             }
         }
     }
