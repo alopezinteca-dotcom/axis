74761277ffdc1fe06c5fa0023b8bd947e451b290
alopezinteca-dotcom <alopezinteca@gmail.com>
2026-04-11 14:09:29 +0200

    Add plugin versions to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    // ✅ Versiones de plugins aquí (CI-safe)
+    plugins {
+        id("com.android.application") version "8.2.2"
+        id("org.jetbrains.kotlin.android") version "1.9.24"
+        id("org.jetbrains.kotlin.kapt") version "1.9.24"
+    }
 }
 
 dependencyResolutionManagement {
