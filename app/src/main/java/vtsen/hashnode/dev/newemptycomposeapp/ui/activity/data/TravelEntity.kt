28f7aea08113a2cf1c093caec3c30eae1095ddc5
alopezinteca-dotcom <alopezinteca@gmail.com>
2026-04-11 15:56:25 +0200

    Update TravelEntity.kt

--- app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/activity/data/TravelEntity.kt
+++ app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/activity/data/TravelEntity.kt
@@ -3,26 +3,26 @@ package vtsen.hashnode.dev.newemptycomposeapp.ui.activity.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "travel")
+@Entity(tableName = "travels")
 data class TravelEntity(
     @PrimaryKey(autoGenerate = true)
-    val id: Long = 0,
+    val id: Long = 0L,
 
     val origin: String,
     val destination: String,
     val description: String,
+
     val kmStart: Int,
     val kmEnd: Int?,
 
-    /**
-     * Facturación prevista del viaje
-     */
     val billingExpected: Double,
-
     val hasDiet: Boolean,
+
+    // Dato maestro al cerrar
     val hoursImputed: Double?,
 
     val status: TravelStatus,
+
     val startTimestamp: Long,
     val endTimestamp: Long?
 )
