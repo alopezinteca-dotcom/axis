44e2521de0f2e6163de54e38bd0f2db07f7830bf
alopezinteca-dotcom <alopezinteca@gmail.com>
2026-04-12 11:24:48 +0200

    Update TravelStatus.kt

--- app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/activity/data/TravelStatus.kt
+++ app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/activity/data/TravelStatus.kt
@@ -1,9 +1,5 @@
 package vtsen.hashnode.dev.newemptycomposeapp.ui.activity.data
 
-/* =========================================================
-   ESTADO DEL VIAJE
-   ========================================================= */
-
 enum class TravelStatus {
     IN_PROGRESS,
     CLOSED
