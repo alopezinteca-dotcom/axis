38ec733ae8f03b2de9ffdc07dd60e792cb39549e
alopezinteca-dotcom <alopezinteca@gmail.com>
2026-04-16 18:15:39 +0200

    Update ActivityViewModel.kt

--- app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/activity/ActivityViewModel.kt
+++ app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/activity/ActivityViewModel.kt
@@ -250,6 +250,12 @@ class ActivityViewModel(
         viewModelScope.launch { stopRepository.delete(stopId) }
     }
 
+    fun updateTravel(updated: vtsen.hashnode.dev.newemptycomposeapp.ui.activity.data.TravelEntity) {
+    viewModelScope.launch {
+        repository.insertTravel(updated) // REPLACE por id
+    }
+}
+
     // =========================
     // 5) VIAJES
     // =========================
